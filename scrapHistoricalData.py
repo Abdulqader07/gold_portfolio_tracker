81fb2d2f3781bf536bf8d4f1326dbcb0d79ddfbc
Abdulqader07 <prejuse.ix@gmail.com>
2025-12-07 00:01:36 +0300

    Changes in the way of how the data will be stored adding error handling and containing the case of updated data

--- scrapHistoricalData.py
+++ scrapHistoricalData.py
@@ -1,16 +1,16 @@
+import os
 import json
 import sqlite3
 import requests
 from datetime import datetime
 
-def histScraper():
-	connection = sqlite3.connect('./database/database.db')
-	cursor = connection.cursor()
+PATH = './database/database.db'
 
+def fn_historicalData():
 	url = 'https://www.macrotrends.net/economic-data/2627/10YD'
 
 	headers = {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0'
+			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0'
 	}
 
 	response = requests.get(url, headers=headers)
@@ -18,17 +18,66 @@ def histScraper():
 	jsonResponse = response.json()
 	data = jsonResponse['data']
 
-	appendData = list()
+	if not os.path.exists(PATH):
 
-	for i in range(len(data)):
-		price = float(data[i][1])
-		date = str(data[i][0])
-		readableDate = datetime.fromtimestamp(int(date[:-3])).strftime("%Y-%m-%d")
+		connection = sqlite3.connect(PATH)
+		cursor = connection.cursor()
 
-		cursor.execute("INSERT INTO historicalData (dateXAU, priceXAU) VALUES (?, ?)",
-			(readableDate, price))
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS historicalData(
+				dateXAU DATE PRIMARY KEY NOT NULL,
+				priceXAU FLOAT NOT NULL
+			);''')
+
+		dataLength = len(data)
+
+		for i in range(dataLength):
+			price = float(data[i][1])
+			date = str(data[i][0])
+			readableDate = datetime.fromtimestamp(int(date[:-3])).strftime("%Y-%m-%d")
+
+			cursor.execute("INSERT INTO historicalData (dateXAU, priceXAU) VALUES (?, ?);",
+				(readableDate, price))
+
+		connection.commit()
+		connection.close()
+
+		return 'DataBase Created With XAU Data.'
+
+	connection = sqlite3.connect(PATH)
+	cursor = connection.cursor()
+
+	cursor.execute('''
+		SELECT dateXAU FROM historicalData 
+		ORDER BY dateXAU DESC LIMIT 1;
+		''')
+
+	lastDateXAU = cursor.fetchone()
+
+	lastJsonDateStr = str(data[-1][0])
+	lastJsonDate = datetime.fromtimestamp(int(lastJsonDateStr[:-3])).strftime('%Y-%m-%d')
+
+	datesList = list()
+
+	while lastJsonDate > lastDateXAU[0]:
+		priceJson = float(data[-1][1])
+		datesList.append((lastJsonDate, priceJson))
+		data.pop()
+		
+		if not data:
+			break
+
+		lastJsonDateStr = str(data[-1][0])
+		lastJsonDate = datetime.fromtimestamp(int(lastJsonDateStr[:-3])).strftime('%Y-%m-%d')
+
+	datesList.reverse()
+
+	for (date, price) in datesList:
+		cursor.execute('''
+			INSERT INTO historicalData (dateXAU, priceXAU) 
+			VALUES (?, ?);''', (date, price))
 
 	connection.commit()
-	connection.close()
+	cursor.close()
 
-	return None
+	return 'DataBase Updated, (New Data Found).'
