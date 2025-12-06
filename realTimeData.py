9e5c3b3cea0b2f2542834211c492cde6f3f8bb78
Abdulqader07 <prejuse.ix@gmail.com>
2025-12-07 00:02:04 +0300

    Changes in the function name

--- realTimeData.py
+++ realTimeData.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def realTimePrice():
+def fn_realTimePrice():
 	url = 'https://www.investing.com/currencies/xau-usd'
 	headers = {
 		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0'
